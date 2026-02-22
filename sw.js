5099de404f8b92724b00daa1f1d403bc8d847d13
ireikk <ireikk@gmail.com>
2026-02-22 17:41:30 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "drink-pwa-v2";
+const CACHE_NAME = "drink-pwa-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -29,3 +29,4 @@ self.addEventListener("fetch", (e) => {
     caches.match(e.request).then((r) => r || fetch(e.request))
   );
 });
+
